bb297856db1d8fc4b62ca9f8d550a4d138c7db99
Park Jeong Woo <frjufvjn@gmail.com>
2021-02-18 15:48:28 +0900

    Create tcagntClient.java
    기존 tcagntClient에서 VAS 채널알람 발생시 한개의 채널감지시 바로 알람을 발생시키지 않고, 임계치 갯수에 도달했을때 알람 / 해제 를 보내도록 적용한 소스 코드

---
+++ tcagntClient.java
@@ -0,0 +1,133 @@
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class tcagntClient {
+  
+  private static boolean isThrottleByNums = false;
+  private static int throttleByNumsThreshold = 15;
+  private static String throttleByNumsKeyword = "";
+  private static int throttleByNumsChannelLimit = 30;
+  private static String throttleByNumsCleanKeyword = "";
+  private static String throttleByNumsClearPrefix = "";
+
+  // <채널번호,클리어여부>
+  private static ConcurrentHashMap<String,Boolean> throttleByNumsInfo = new ConcurrentHashMap<String,Boolean>();
+  
+  // 생성자
+  public tcagntClient {
+    String throttle_by_nums_yn = getProperty("THROTTLE_BY_NUMS_YN");
+    if (throttle_by_nums_yn != null && "Y".equals(throttle_by_nums_yn)) {
+      isThrottleByNums = true;
+    }
+    
+    String throttle_by_nums_threshold = getProperty("THROTTLE_BY_NUMS_THRESHOLD");
+    if (throttle_by_nums_threshold != null) {
+      throttleByNumsThreshold = Integer.parseInt(throttle_by_nums_threshold);
+    }
+    
+    String throttle_by_nums_keyword = getProperty("THROTTLE_BY_NUMS_KEYWORD");
+    if (throttle_by_nums_keyword != null) {
+      throttleByNumsKeyword = throttle_by_nums_keyword;
+    }
+    
+    String throttle_by_nums_channel_limit = getProperty("THROTTLE_BY_NUMS_CHANNEL_LIMIT");
+    if (throttle_by_nums_channel_limit != null) {
+      throttleByNumsChannelLimit = throttle_by_nums_channel_limit;
+    }
+    
+    String throttle_by_nums_clear_keyword = getProperty("THROTTLE_BY_NUMS_CLEAR_KEYWORD");
+    if (throttle_by_nums_clear_keyword != null) {
+      throttleByNumsCleanKeyword = throttle_by_nums_clear_keyword;
+    }
+    
+    String throttle_by_nums_clear_prefix = getProperty("THROTTLE_BY_NUMS_CLEAR_PREFIX");
+    if (throttle_by_nums_clear_prefix != null) {
+      throttleByNumsClearPrefix = throttle_by_nums_clear_prefix;
+    }
+    
+    sysout(ALL PROPERTIES....);
+    
+  }
+  
+  class Display implements Runnable {
+    
+    public void run() {
+      ...
+      for (int j=0; j < crit_log_pattern.size(); j++) {
+
+        String critKey = (String) crit_log_pattern.get(j);
+        if ( s3.indexOf((String) critKey) >= 0 ) {
+          msg = critKey + "::" + s3;
+          if ( !isThrottleByNums ) {
+            // #---------------------------------------------------
+            // 채널 알람이 아닌 일반 
+            // #---------------------------------------------------
+            new Thread(new Handler(msg)).start();
+          } else {
+            //#-------------------------------------------------------
+            // PJW 2021-02-18 VAS 채널알람 발생 제어
+            //#-------------------------------------------------------
+            if (s3.indexOf(throttleByNumsKeyword) != -1) {
+              String[] s3Arr = s3.split(" ");
+              String chanStr = "";
+              
+              for (int i=0; i <s3Arr.length; i++) {
+                if (s3Arr[i].indexOf("[") != -1 && s3Arr[i].indexOf("]") != -1  ) {
+                  String tmpStr = s3Arr[i].substring((s3Arr[i].indexOf("[") + 1), s3Arr[i].indexOf("]"));
+                  String _chanStr = tmpStr.replace("[","").replace("[","");
+                  try {
+                    int chanNum = Integer.parseInt(_chanStr);
+                    if ( chanNum <= throttleByNumsChannelLimit ) {
+                      chanStr = _chanStr;
+                    }
+                    break;
+                    
+                  } catch(NumberFormatException e) {
+                    continue;
+                  }
+                }
+              }
+              
+              if (!"".equals(chanStr)) {
+                System.out.println("VAS CHANNEL EVT...");
+                if (s3.indexOf(throttleByNumsClearKeyword) != -1) {
+                  throttleByNumsInfo.put(chanStr, true);
+                } else {
+                  throttleByNumsInfo.put(chanStr, false);
+                }
+                
+                int currentFailChannelCnt = 0; // 현재 Fail 상태인 채널 갯수
+                int currentClearChannelCnt = 0; // 현재 Clear된 상태인 채널 갯수
+                Set<Entry<String,Boolean>> entry = throttleByNumsInfo.entrySet();
+                for (Entry<String,Boolean> element : entry) {
+                  if (!element.getValue()) {
+                    currentFailChannelCnt++;
+                  } else {
+                    currentClearChannelCnt++;
+                  }
+                }
+                System.out.println("FAIL CNT:" + currentFailChannelCnt + " CLEAR CNT:" + currentClearChannelCnt);
+                
+                if ( currentFailChannelCnt >= throttleByNumsThreshold ) {
+                  new Thread(new Handler(msg)).start();
+                }
+                
+                if ( currentClearChannelCnt >= throttleByNumsThreshold ) {
+                  // 해제알람인지 인지 시키기 위해서 정의함. 텔레캡스 어드민에서도 prefix를 동일하게 등록시켜줘야함.
+                  final String chearPrefix = throttleByNumsClearPrefix;
+                  new Thread(new Handler(chearPrefix + msg)).start();
+                }
+              }
+              
+            } else {
+              // #---------------------------------------------------
+              // 채널 알람이 아닌 일반 
+              // #---------------------------------------------------
+              new Thread(new Handler(msg)).start();
+            }
+          }
+        }
+      }
+    }
+  }
+}
